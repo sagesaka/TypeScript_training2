c2467e8f7deeee72ba59bea667f0a46d1ce999cd
sagesaka <sagesaka@riprice.co.jp>
2024-09-10 17:46:43 +0900

    型定義を勉強

---
+++ type.ts
@@ -0,0 +1,15 @@
+const Num: number = 3;
+const str1: string = "Hello1";
+const str2: string = 'Hello2';
+const str3: string = `Hello3 ${str2}`;
+const flg: boolean = true;
+const big: bigint = 1000n;
+const val: null = null;
+const val2: undefined = undefined;
+
+console.log(Num);
+// typeofで型を出力
+console.log(typeof str1);
+console.log(typeof str2, str2);
+
+
